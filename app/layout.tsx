e92d1fbe7415cb1c3b9b2644d0f1957d6cd2d052
firekylin2024 <161567010+firekylin2024@users.noreply.github.com>
2025-06-29 19:45:09 +0800

    更新clarity追踪代码
    改了域名，相应追踪代码改一下，因为最后一小节数字变了

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,10 +31,15 @@ export default function RootLayout({
 
         <Script
           id="microsoft-clarity-script"
+          type="text/javascript"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "ru78p8nray");
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "s71uqkmvb5");
             `,
           }}
         />
